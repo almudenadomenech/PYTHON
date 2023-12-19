4c5cdd6a9d98b31d82499c4894a17691906aa3c6
almudenadomenech <violeta_33@hotmail.es>
2023-12-19 11:31:23 +0100

    mas funciones

--- python/031.funciones_import.py
+++ python/031.funciones_import.py
@@ -1,5 +1,8 @@
  
-# from nombrearchivo import funcion/variable
-from saludos import saludo1
+# from nombrearchivo import funcion/variable/objetos/clase
+# En este opción se importa solamento lo que se necesita
+from saludos import saludo1, saludo2
 
 saludo1()
+
+saludo2('prueba')
---
+++ python/033.funciones_import_as.py
@@ -0,0 +1,11 @@
+# Utilizamos as para abreviar
+
+import saludos as sa
+
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+
+sa.saludo1()
